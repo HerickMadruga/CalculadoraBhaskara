42b44df29f382d4b58ddb779a0d7566ca6aa3812
Herick <herickmadruga8@gmail.com>
2025-06-20 05:19:04 -0300

    Primeiro Commit

---
+++ CalculadoraBb.js
@@ -0,0 +1,22 @@
+function pegar(){
+    let a = Number(document.querySelector(".A").value);
+    let b = Number(document.querySelector(".B").value);
+    let c = Number(document.querySelector(".C").value);
+    
+    let delta = (b**2) - (4*a*c);
+    if(delta<0)
+    {
+        alert('X impossivel nos reais');
+    }
+    else if(delta >= 0 )
+    {
+        let x1= (-b+(Math.sqrt(delta)))/(2*a);
+        let x2= (-b-(Math.sqrt(delta)))/(2*a);
+        document.querySelector(".X1").textContent = "x1 é: " + x1; 
+        document.querySelector(".X2").textContent = "x2 é: " + x2; 
+    }
+
+}
+window.onload = function(){
+    pegar();
+};
